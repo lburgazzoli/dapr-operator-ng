03f8d404b0c7fae04b24863ace035f654a780c7d
Luca Burgazzoli <lburgazzoli@gmail.com>
2023-08-12 17:33:37 +0200

    test

--- test/e2e/dapr_deploy_test.go
+++ test/e2e/dapr_deploy_test.go
@@ -1,12 +1,13 @@
 package e2e
 
 import (
-	daprApi "github.com/lburgazzoli/dapr-operator-ng/api/dapr/v1alpha1"
+	"testing"
+
+	daprAc "github.com/lburgazzoli/dapr-operator-ng/pkg/client/dapr/applyconfiguration/dapr/v1alpha1"
 	"github.com/lburgazzoli/dapr-operator-ng/pkg/pointer"
 	"github.com/onsi/gomega"
 	"github.com/rs/xid"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"testing"
 
 	. "github.com/lburgazzoli/dapr-operator-ng/test/support"
 )
@@ -18,15 +19,15 @@ func TestDaprDeploy(t *testing.T) {
 	ns := test.NewTestNamespace()
 	dp := test.Client().Dapr.DaprV1alpha1().Daprs(ns.Name)
 
-	instance, err := dp.Create(
+	instance, err := dp.Apply(
 		test.Ctx(),
-		&daprApi.Dapr{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      xid.New().String(),
-				Namespace: ns.Name,
-			},
-		},
-		metav1.CreateOptions{})
+		daprAc.Dapr(xid.New().String(), ns.Name).
+			WithSpec(daprAc.DaprSpec().
+				WithValues(nil),
+			),
+		metav1.ApplyOptions{
+			FieldManager: "dapr-test",
+		})
 
 	test.T().Cleanup(func() {
 		test.Expect(
