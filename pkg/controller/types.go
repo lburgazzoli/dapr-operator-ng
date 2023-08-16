268c9c823215292813b60eb24d23e8f23b0f33a2
Luca Burgazzoli <lburgazzoli@gmail.com>
2023-08-16 11:33:34 +0200

    refactor

--- pkg/controller/types.go
+++ pkg/controller/types.go
@@ -1,28 +1,17 @@
 package controller
 
-import "time"
-
 type ClusterType string
 
 const (
 	ClusterTypeVanilla   ClusterType = "Vanilla"
 	ClusterTypeOpenShift ClusterType = "OpenShift"
 
-	KubernetesLabelAppName      = "app.kubernetes.io/name"
-	KubernetesLabelAppInstance  = "app.kubernetes.io/instance"
-	KubernetesLabelAppComponent = "app.kubernetes.io/component"
-	KubernetesLabelAppPartOf    = "app.kubernetes.io/part-of"
-	KubernetesLabelAppManagedBy = "app.kubernetes.io/managed-by"
-
 	DaprReleaseGeneration = "daprs.tools.dapr.io/release.generation"
 	DaprReleaseName       = "daprs.tools.dapr.io/release.name"
 	DaprReleaseNamespace  = "daprs.tools.dapr.io/release.namespace"
 
-	SyncInterval     = 5 * time.Second
-	RetryInterval    = 10 * time.Second
-	ConflictInterval = 1 * time.Second
-	FinalizerName    = "tools.dapr.io/finalizer"
-	FieldManager     = "dapr-release-controller"
+	FinalizerName = "tools.dapr.io/finalizer"
+	FieldManager  = "dapr-release-controller"
 )
 
 type Options struct {
